d4db031ce26ece6ea8830899e69f40fb6989f357
rlagotqks081 <khbkss77@naver.com>
2026-06-16 21:43:30 +0900

    [feat] 아이템 데이터베이스 구현 / 상점 팝업에 아이템 슬롯 동적 생성 / 아이템 구매 기능 구현(임시)
    Goat_Store Scene에서 테스트 해보시면 됩니당

---
+++ Assets/Scripts/Data/ItemDataBase.cs
@@ -0,0 +1,70 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EffectType
+{
+    UpgradeHealth, SpeedUp, MiniGamePointDouble
+}
+
+public abstract class ItemBase : ScriptableObject
+{
+    [SerializeField] public int Price;
+    [SerializeField] public string Name;
+    [SerializeField] public string Iconpath;
+
+    public virtual bool Buy()
+    {
+        if (StoreManager.Instance._coins >= Price)
+        {
+            StoreManager.Instance.SpendCoins(Price);
+            return true;
+        }
+        return false;
+    }
+}
+
+public class ItemDataBase  // 소모템, 치장템은 아직 추가 안했음
+{
+    public static ItemDataBase Instance = new ItemDataBase();
+
+    public List<PermanentItem> PermanentList = new List<PermanentItem>();
+
+    public void LoadAllItems()  
+    {
+        PermanentItem[] loadedPermanentItems = Resources.LoadAll<PermanentItem>("StoreItemDatas/PermanentItem");
+
+        foreach (PermanentItem item in loadedPermanentItems)
+        {
+            PermanentList.Add(item);
+        }
+        Debug.Log($"[ItemDatabase] Item 로드 완료, 총 {PermanentList.Count} 개 ");
+    }
+}
+
+
+
+// 영구적인 효과 아이템 데이터
+[CreateAssetMenu(fileName = "NewPermanentItem", menuName = "ScriptableObject/Permanent")]
+public class PermanentItem : ItemBase
+{
+    public EffectType effectType;
+}
+
+// 치장아이템 데이터, 사용할지는 모르겠음
+[CreateAssetMenu(fileName = "NewCosmeticItem", menuName = "ScriptableObject/Cosmetic")]
+public class CosmeticItem : ItemBase
+{
+
+    // 치장 관련 변수 적기
+}
+
+// 소모품아이템 데이터, 사용할지는 모르겠음
+[CreateAssetMenu(fileName = "NewConsumableItem", menuName = "ScriptableObject/Consumable")]
+public class ConsumableItem : ItemBase
+{ 
+
+    // 소모품 관련 변수 적기
+}
+
+
--- Assets/Scripts/Manager/StoreManager.cs
+++ Assets/Scripts/Manager/StoreManager.cs
@@ -1,15 +1,36 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class StoreManager : BaseMonoManager<StoreManager>
 {
+    [Header("Prefab")]
+    [SerializeField] private GameObject _storeItems;
+
     [Header ("Store Popup")]
     [SerializeField] private GameObject _storePopup;
     [SerializeField] private Button _exitButton;
+    [SerializeField] private TextMeshProUGUI _coinText;
+    [SerializeField] private Transform _contentParent;
 
     private void OnEnable()
     {
         _exitButton.onClick.AddListener(OnClickExitBtn);
+        ItemDataBase.Instance.LoadAllItems();
+        InitStorePopup();
+    }
+
+    private void InitStorePopup()
+    {
+        foreach (Transform child in _contentParent)
+            Destroy(child.gameObject);
+
+        foreach (var item in ItemDataBase.Instance.PermanentList)
+        {
+            GameObject slotObj = Instantiate(_storeItems, _contentParent);
+
+            slotObj.GetComponent<StoreItemSlot>().Setup(item);
+        }
     }
 
     public void OnClickExitBtn()
@@ -21,14 +42,57 @@ public class StoreManager : BaseMonoManager<StoreManager>
 
     public void OpenStorePopup()
     {
+        UpdateStorePopup();
+
         _storePopup.SetActive(true);
 
         SetCursorState(true);
     }
 
+    public void HandlePurchase(ItemBase itemData, Button button)
+    {
+        if(!itemData.Buy())
+        {
+            // 코인이 부족하다는 메시지 띄우기
+            return;
+        }
 
 
-    ////// 아래는 임시로 만든 함수 (다른곳에서 만들어지면 지울 예정)
+        if (itemData is PermanentItem data)  // 일단은 영구적인 효과의 아이템만 구현
+        {
+            UpdateStorePopup();
+            button.interactable = false;
+            switch (data.effectType)
+            {
+                case EffectType.UpgradeHealth:
+                    Debug.Log("UpgradeHealth 구매됨");
+                    break;
+                case EffectType.SpeedUp:
+                    Debug.Log("SpeedUp 구매됨");
+                    break;
+                case EffectType.MiniGamePointDouble:
+                    Debug.Log("MiniGamePointDouble 구매됨");
+                    break;
+            }
+        }
+
+    }
+
+
+
+
+    ////// 아래는 임시로 만든 함수 or 변수 (다른곳에서 만들어지면 지울 예정)
+    
+    //임시 소유 코인
+    public int _coins = 999999;
+
+    public void SpendCoins(int amount)
+    {
+        if(_coins >= amount)
+        {
+            _coins -= amount;
+        }
+    }
 
     //임시 마우스커서 활성화/비활성화 함수
     public void SetCursorState(bool state)
@@ -38,4 +102,10 @@ public class StoreManager : BaseMonoManager<StoreManager>
         if (state) Cursor.lockState = CursorLockMode.None;
         else Cursor.lockState = CursorLockMode.Locked;
     } 
+
+    //임시 팝업 UI 업데이트
+    public void UpdateStorePopup()
+    {
+        _coinText.text = _coins.ToString();
+    }
 }
