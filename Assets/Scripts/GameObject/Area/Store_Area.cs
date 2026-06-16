4054418ec93b0ac3e5dd98457f523c32a1466a32
rlagotqks081 <khbkss77@naver.com>
2026-06-16 18:04:47 +0900

    [init][feat]기본 설정 / StoreArea, StorePopup 프리팹, StoreManager 기본 틀 구현

---
+++ Assets/Scripts/GameObject/Area/Store_Area.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Store_Area : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StoreManager.Instance.OpenStorePopup();
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StoreManager.Instance.OnClickExitBtn();
+        }
+    }
+}
---
+++ Assets/Scripts/Manager/StoreManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class StoreManager : BaseMonoManager<StoreManager>
+{
+    [Header ("Store Popup")]
+    [SerializeField] private GameObject _storePopup;
+    [SerializeField] private Button _exitButton;
+
+    private void OnEnable()
+    {
+        _exitButton.onClick.AddListener(OnClickExitBtn);
+    }
+
+    public void OnClickExitBtn()
+    {
+        _storePopup.SetActive(false);
+
+        SetCursorState(false);
+    }
+
+    public void OpenStorePopup()
+    {
+        _storePopup.SetActive(true);
+
+        SetCursorState(true);
+    }
+
+
+
+    ////// 아래는 임시로 만든 함수 (다른곳에서 만들어지면 지울 예정)
+
+    //임시 마우스커서 활성화/비활성화 함수
+    public void SetCursorState(bool state)
+    {
+        Cursor.visible = state;
+
+        if (state) Cursor.lockState = CursorLockMode.None;
+        else Cursor.lockState = CursorLockMode.Locked;
+    } 
+}
