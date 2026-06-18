d7094a43396ea732bf133df0ed976e5d7cf4720c
현진 <hiimhoihoi@gmail.com>
2026-06-18 13:26:19 +0900

    [feat] 낮밤 시간 흐름 관리 추가
    - DayPhase enum 추가
    - 낮 시간 제한 및 남은 시간 관리 기능 추가
    - 낮 시간이 종료되면 밤 상태로 전환되도록 구현

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,24 +1,43 @@
 ﻿using System;
 using UnityEngine;
-    public enum GameState
-    {
-        None = 0,
-        Loading,
-        Ready,
-        Playing,
-        Pause,
-        GameOver
-    }
+
+public enum GameState
+{
+    None = 0,
+    Loading,
+    Ready,
+    Playing,
+    Pause,
+    GameOver
+}
+
+public enum DayPhase
+{
+    None = 0,
+    Day,
+    Night
+}
 
 public class GameManager : BaseMonoManager<GameManager>
 {
+    [SerializeField] private float _dayDuration = 300f;
+
+    private float _remainDayTime;
+
     public GameState CurrentState { get; private set; } = GameState.None;
+    public DayPhase CurrentDayPhase { get; private set; } = DayPhase.None;
 
     public bool IsPlaying => CurrentState == GameState.Playing;
     public bool IsPaused => CurrentState == GameState.Pause;
     public bool IsGameOver => CurrentState == GameState.GameOver;
 
+    public float RemainDayTime => _remainDayTime;
+    public float DayDuration => _dayDuration;
+    public float DayTimeRate => _dayDuration <= 0f ? 0f : _remainDayTime / _dayDuration;
+
     public event Action<GameState> OnGameStateChanged;
+    public event Action<DayPhase> OnDayPhaseChanged;
+    public event Action<float> OnDayTimeChanged;
 
     protected override void Awake()
     {
@@ -30,11 +49,17 @@ public class GameManager : BaseMonoManager<GameManager>
         InitializeGame();
     }
 
+    private void Update()
+    {
+        UpdateDayTime();
+    }
+
     private void InitializeGame()
     {
         ChangeGameState(GameState.Loading);
 
-        // TODO : 매니저 초기화 예정
+        _remainDayTime = _dayDuration;
+        ChangeDayPhase(DayPhase.Day);
 
         ChangeGameState(GameState.Ready);
     }
@@ -48,6 +73,13 @@ public class GameManager : BaseMonoManager<GameManager>
 
         Time.timeScale = 1f;
         ChangeGameState(GameState.Playing);
+    }
+
+    public void StartDay()
+    {
+        _remainDayTime = _dayDuration;
+        ChangeDayPhase(DayPhase.Day);
+        OnDayTimeChanged?.Invoke(_remainDayTime);
     }
 
     public void PauseGame()
@@ -94,5 +126,39 @@ public class GameManager : BaseMonoManager<GameManager>
 
         Debug.Log($"게임 상태 변경됨 > {CurrentState}");
         OnGameStateChanged?.Invoke(CurrentState);
+    }
+
+    public void ChangeDayPhase(DayPhase dayPhase)
+    {
+        if (CurrentDayPhase == dayPhase)
+        {
+            return;
+        }
+
+        CurrentDayPhase = dayPhase;
+        OnDayPhaseChanged?.Invoke(CurrentDayPhase);
+    }
+
+    private void UpdateDayTime()
+    {
+        if (!IsPlaying)
+        {
+            return;
+        }
+
+        if (CurrentDayPhase != DayPhase.Day)
+        {
+            return;
+        }
+
+        _remainDayTime -= Time.deltaTime;
+        _remainDayTime = Mathf.Max(_remainDayTime, 0f);
+
+        OnDayTimeChanged?.Invoke(_remainDayTime);
+
+        if (_remainDayTime <= 0f)
+        {
+            ChangeDayPhase(DayPhase.Night);
+        }
     }
 }
