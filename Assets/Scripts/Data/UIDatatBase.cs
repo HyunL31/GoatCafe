e61cb9dd0f846d501655b27da612adfd7c98e9bb
이기웅 <littlemanner1@gmail.com>
2026-06-18 16:59:58 +0900

    [fix] 실수로 누락한 파일 커밋

---
+++ Assets/Scripts/Data/UIDatatBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+[Serializable]
+public class UIDataBase
+{
+    public string Id;
+}
+
+public class MainMenuUIData : UIDataBase
+{
+    public string StartButton;
+    public string GameOptionButton;
+    public string ExitGameButton;
+}
