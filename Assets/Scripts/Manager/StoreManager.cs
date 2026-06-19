a8b99064a8937b6f901db7a78c3a16390226235b
김현빈 <khbkss77@naver.com>
2026-06-19 15:20:02 +0900

    [feat] 치장품(Crown) 구현 완료
    테스트는 Goat_Store

--- Assets/Scripts/Manager/StoreManager.cs
+++ Assets/Scripts/Manager/StoreManager.cs
@@ -1,18 +1,85 @@
-﻿using TMPro;
+﻿using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class StoreManager : BaseMonoManager<StoreManager>
 {
+
+    //임시 소유 코인
+    public int _coins = 999999;
+
     [Header("Prefab")]
     [SerializeField] private GameObject _storeItems;
 
-    [Header ("Store Popup")]
+    [Header("Store Popup")]
     [SerializeField] private GameObject _storePopup;
     [SerializeField] private Button _exitButton;
     [SerializeField] private TextMeshProUGUI _coinText;
     [SerializeField] private Transform _contentParent;
 
+    [Header("Cosmetic Object")]
+    [SerializeField] private GameObject _crown;
+    [SerializeField] private GameObject _humanoidCrown;
+
+    private HashSet<ItemBase> purchasedItems = new HashSet<ItemBase>();  // 구매한 영구적/치장 아이템 보관
+    private HashSet<ItemBase> equippedItems = new HashSet<ItemBase>();  // 치장 아이템 보관
+
+    public bool IsPurchased(ItemBase itemData) => purchasedItems.Contains(itemData);
+    public void AddPurchased(ItemBase itemData)
+    {
+        if (!purchasedItems.Contains(itemData))
+        {
+            purchasedItems.Add(itemData);
+        }
+    }
+    public void RemovePurchased(ItemBase itemData)
+    {
+        if(purchasedItems.Contains(itemData))
+        {
+            purchasedItems.Remove(itemData);
+        }
+    }
+    public bool IsEquipped(ItemBase itemData) => equippedItems.Contains(itemData);
+    public void AddEquipped(ItemBase itemData)
+    {
+        if (!equippedItems.Contains(itemData))
+        {
+            equippedItems.Add(itemData);
+        }
+    }
+    public void RemoveEquipped(ItemBase itemData)
+    {
+        if (equippedItems.Contains(itemData))
+        {
+            equippedItems.Remove(itemData);
+        }
+    }
+    private Dictionary<ItemBase, int> inventoryDic = new Dictionary<ItemBase, int>();
+    public void AddInventory(ItemBase itemData)
+    {
+        if(inventoryDic.ContainsKey(itemData))
+        {
+            inventoryDic[itemData] += 1;
+        }
+        else
+        {
+            inventoryDic.Add(itemData, 1);
+        }
+    }
+
+    public void UseInventoryItem(ItemBase itemData)
+    {
+        if(inventoryDic.ContainsKey(itemData))
+        {
+            inventoryDic[itemData] -= 1;
+        }
+        if (inventoryDic[itemData] <= 0)
+        {
+            inventoryDic.Remove(itemData);
+        }
+    }
 
     private void OnEnable()
     {
@@ -38,6 +105,12 @@ public class StoreManager : BaseMonoManager<StoreManager>
 
             slotObj.GetComponent<StoreItemSlot>().Setup(item);
         }
+        foreach (var item in ItemDataBase.Instance.CosmeticList)
+        {
+            GameObject slotObj = Instantiate(_storeItems, _contentParent);
+
+            slotObj.GetComponent<StoreItemSlot>().Setup(item);
+        }
     }
 
     public void OnClickExitBtn()
@@ -56,19 +129,28 @@ public class StoreManager : BaseMonoManager<StoreManager>
         SetCursorState(true);
     }
 
-    public void HandlePurchase(ItemBase itemData, Button button)
+    public void HandleButtonClick(ItemBase itemData, Button button)
     {
-        if(!itemData.Buy())
+        if (!purchasedItems.Contains(itemData)) 
         {
-            // 코인이 부족하다는 메시지 띄우기
-            return;
+            if (!itemData.Buy())
+            {
+                // 돈 부족하다는 메시지 띄우기
+                Debug.Log("[StoreManager] Coin 부족");
+                return;
+            }
+            else
+            {
+                AddPurchased(itemData);
+            }
         }
         UpdateStorePopup();
 
         if (itemData is PermanentItem permanentData)  // 일단은 영구적인 효과의 아이템만 구현
         {
             button.interactable = false;
-            switch (permanentData.effectType)
+
+            switch (permanentData.effectType)   // 영구적 아이템 효과 적용 부분
             {
                 case EffectType.UpgradeHealth:
                     Debug.Log("UpgradeHealth 구매됨");
@@ -81,20 +163,54 @@ public class StoreManager : BaseMonoManager<StoreManager>
                     break;
             }
         }
-        if(itemData is ConsumableItem consumabledata)
-        {
 
+
+        if(itemData is ConsumableItem consumableData)
+        {
+            if(inventoryDic.ContainsKey(consumableData))
+            {
+                inventoryDic[consumableData] += 1;
+            }
+            else
+            {
+                inventoryDic.Add(consumableData, 1);
+            }
         }
 
+        if(itemData is CosmeticItem cosmeticData)
+        {
+            button.transform.Find("Store_GoldIcon").gameObject.SetActive(false);
+            if (equippedItems.Contains(itemData))
+            {
+                equippedItems.Remove(itemData);
+                SetCosmetic(cosmeticData.type, false);
+                button.GetComponentInChildren<TextMeshProUGUI>().text = "Equip";
+            }
+            else
+            {
+                equippedItems.Add(itemData);
+                SetCosmetic(cosmeticData.type, true);
+                button.GetComponentInChildren<TextMeshProUGUI>().text = "Unequip";
+            }
+
+        }
     }
 
-
+    private void SetCosmetic(CosmeticType type, bool isActive)
+    {
+        switch(type)
+        {
+            case CosmeticType.Crown:
+                _crown.SetActive(isActive);
+                _humanoidCrown.SetActive(isActive);
+                break;
+        }
+    }
 
 
     ////// 아래는 임시로 만든 함수 or 변수 (다른곳에서 만들어지면 지울 예정)
     
-    //임시 소유 코인
-    public int _coins = 999999;
+
 
     public void SpendCoins(int amount)
     {
