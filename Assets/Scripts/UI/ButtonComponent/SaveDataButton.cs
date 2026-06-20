16b82880d9f98056429e51d6720568ca850320fd
이기웅 <littlemanner1@gmail.com>
2026-06-20 12:12:35 +0900

    [feat] Master 최신화

---
+++ Assets/Scripts/UI/ButtonComponent/SaveDataButton.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class SaveDataButton : BaseButton
+{
+    [SerializeField] private Image Image_Background;
+
+    [SerializeField] private TMP_Text Text_DataName;
+
+    [SerializeField] private TMP_Text Text_DayTitle;
+    [SerializeField] private TMP_Text Text_GoldTitle;
+    [SerializeField] private TMP_Text Text_StaminaTitle;
+
+    [SerializeField] private TMP_Text Text_DayData;
+    [SerializeField] private TMP_Text Text_GoldData;
+    [SerializeField] private TMP_Text Text_StaminaData;
+
+    private void Awake()
+    {
+        OnSetDataEnded += SetData;
+    }
+
+    private void OnDestroy()
+    {
+        OnSetDataEnded -= SetData;
+    }
+
+    private void SetData()
+    {
+        TMP_FontAsset font = Text_Button.font;
+
+        Text_DataName.font = font;
+
+        Text_DayTitle.font = font;
+        Text_GoldTitle.font = font;
+        Text_StaminaTitle.font = font;
+
+        Text_DayData.font = font;
+        Text_GoldData.font = font;
+        Text_StaminaData.font = font;
+    }
+    
+    public void SetSaveData(PlayerModel playerModel)
+    {
+        Text_DataName.text = "";
+        Text_DayData.text = playerModel.Day.ToString();
+        Text_GoldData.text = playerModel.Gold.ToString();
+        Text_StaminaData.text = playerModel.Stamina.ToString();
+    }
+
+    
+}
---
+++ Assets/Scripts/UI/SaveDataSlotPopup/SaveDataSlotPopup.cs
@@ -0,0 +1,63 @@
+﻿using Cysharp.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+
+public class SaveDataSlotPopupPresenter : BasePresenter<SaveDataSlotPopupPresenter, MainUI>
+{
+    public override UIType UIType_This { get; } = UIType.SaveSlotPopup;
+
+    private MainUI _mainUI;
+
+    private GameObject _prefab_loadButton;
+
+    private Sprite _sprite_background;
+    private Sprite _sprite_titleImage;
+    private Sprite _sprite_saveButtons;
+
+    private TMP_FontAsset _fontAsset_menuFont;
+
+    public override void InitUI(MainUI ui)
+    {
+        _mainUI = ui;
+    }
+
+    protected async override UniTaskVoid SetUI()
+    {
+        await LoadAssetAsync();
+        LoadData();
+
+
+    }
+
+    protected async override UniTask LoadAssetAsync()
+    {
+        var (sprite_background, sprite_titleImage, sprite_saveButton, fontAsset_menuFont) = await UniTask.WhenAll
+            (
+            LoadUtil.Async.LoadSpriteAsync(AddressUtil.Sprite.UI.SaveSlotPopup.Background),
+            LoadUtil.Async.LoadSpriteAsync (AddressUtil.Sprite.UI.SaveSlotPopup.TitleImage),
+            LoadUtil.Async.LoadSpriteAsync(AddressUtil.Sprite.UI.SaveSlotPopup.SaveButton),
+            LoadUtil.Async.LoadFontAssetAsync(AddressUtil.Font.BaseFont)
+            );
+
+        _sprite_background = sprite_background;
+        _sprite_titleImage = sprite_titleImage;
+        _sprite_saveButtons = sprite_saveButton;
+        _fontAsset_menuFont = fontAsset_menuFont;
+
+    }
+
+    protected override void LoadData()
+    {
+        for (int i = 0; i < GameManager.Instance.SlotIndex.Count; i++)
+        {
+            PlayerModel playerModel = SaveManager.Instance.RequestLoadData(GameManager.Instance.SlotIndex[i]);
+        
+            
+        
+        
+        }
+
+
+    }
+
+}
