3343eaa5ab14088ac66fd468c24eb35070ec31de
이강호 <tiger04176672@gmail.com>
2026-06-24 18:21:11 +0900

    [feat] 낮) 카페에 쓰레기가 자동으로 생성되는 기능 구현

---
+++ Assets/Scripts/GameObject/Map/Trash.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System;
+
+public class Trash : MonoBehaviour
+{
+    public static event Action<Trash> OnTrashEnter;
+    public static event Action<Trash> OnTrashExit;
+
+    public static event Action<Trash> OnTrashInteract;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+        if (OnTrashEnter != null)
+            OnTrashEnter(this);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+        if (OnTrashExit != null)
+            OnTrashExit(this);
+    }
+
+    public void Interact()
+    {
+        if (OnTrashInteract != null)
+            OnTrashInteract(this);
+    }
+}
