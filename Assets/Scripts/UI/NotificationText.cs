b9f321929bab13b93cfedc140e3cc498ee9bc545
김현빈 <khbkss77@naver.com>
2026-06-24 17:43:20 +0900

    [feat] NotificationManager 추가 / 텍스트 알림 기능 추가함

---
+++ Assets/Scripts/UI/NotificationText.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using TMPro;
+using DG.Tweening;
+using System;
+
+public class NotificationText : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI messageText;
+    [SerializeField] private CanvasGroup canvasGroup;
+    [SerializeField] private RectTransform rectTransform;
+
+    private Sequence seq;
+
+    public void Setup(string message, Color textColor, float moveY, float duration, Action onCompleteCallback)
+    {
+
+        if(seq != null && seq.IsActive())
+        {
+            seq.Kill();
+        }
+        messageText.text = message;
+        messageText.color = textColor;
+        canvasGroup.alpha = 1f;
+
+        seq = DOTween.Sequence();
+
+        seq.Join(rectTransform.DOAnchorPos(new Vector2(rectTransform.anchoredPosition.x, rectTransform.anchoredPosition.y + moveY), duration)
+            .SetEase(Ease.OutCubic))
+            .SetUpdate(true);
+        seq.Join(canvasGroup.DOFade(0f, duration))
+            .SetUpdate(true);
+
+        seq.SetUpdate(true);
+
+        seq.OnComplete(() => {
+            onCompleteCallback?.Invoke();
+        });
+    }
+}
