bb80891b2cec51dc1f1a43d993b45a62aa0fbb71
이주현 <juhyun040100@gmail.com>
2026-06-29 00:05:23 +0900

    [feat] 엔딩 확인 시 세이브 슬롯 엔딩으로 연결

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -276,10 +276,12 @@ public class GameManager : BaseMonoManager<GameManager>
 
         if (isCoinSuccess && isStolenSuccess)
         {
+            SaveManager.Instance.CurrentPlayerModel.Ending = "Happy_Ending";
             CurrentDialogueID = "Happy_Ending_01";
             return EndingType.HappyEnding;
         }
 
+        SaveManager.Instance.CurrentPlayerModel.Ending = "Bad_Ending";
         CurrentDialogueID = "Bad_Ending_01";
         return EndingType.BadEnding;
     }
@@ -291,6 +293,7 @@ public class GameManager : BaseMonoManager<GameManager>
         Debug.Log($"엔딩 > {endingType}");
 
         OnEndingDetermined?.Invoke(endingType);
+        SaveManager.Instance.SaveData();
         UIManager.Instance.OpenDialogueUI().Forget();
     }
 
